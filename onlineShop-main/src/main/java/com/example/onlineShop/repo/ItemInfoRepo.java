6d461cb5b7c6bfd3fea092bf852d26d39f1705bd
ElizavetaLisenkova <59146630+ElizavetaLisenkova@users.noreply.github.com>
2021-06-22 16:06:49 +0300

    Delete onlineShop-main directory

--- onlineShop-main/src/main/java/com/example/onlineShop/repo/ItemInfoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.onlineShop.repo;
-
-import com.example.onlineShop.entity.ItemInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemInfoRepo extends JpaRepository<ItemInfo, Long> {
-}
--- onlineShop-main/src/main/java/com/example/onlineShop/repo/ManufacturerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.onlineShop.repo;
-
-import com.example.onlineShop.entity.Manufacturer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManufacturerRepo extends JpaRepository<Manufacturer, Long> {
-}
